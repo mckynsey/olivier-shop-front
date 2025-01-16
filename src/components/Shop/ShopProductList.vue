94fb2f78e898f8f6a718448bbf1ff9bae6d00a9c
mckynsey <ayoub.elkaasaoui.etu@univ-lille.fr>
2025-01-16 13:58:58 +0100

    OOOLLIIIVIIIIEEERRRR

---
+++ src/components/Shop/ShopProductList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ShopProduct from "./ShopProduct.vue";
+
+defineProps({
+  productList: Array,
+});
+const emit = defineEmits(["addProductToCart"]); 
+</script>
+
+<template>
+  <div class="grid">
+    <ShopProduct v-for="product of productList"
+     :product="product"
+    
+    
+    @add-product-to-cart="(productId) => emit('addProductToCart', productId)"/>
+    
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-auto-rows: 400px;
+  gap: 20px;
+}
+</style>
